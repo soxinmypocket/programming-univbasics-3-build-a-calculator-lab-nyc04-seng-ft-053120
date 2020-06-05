423392ae5132cd56143f9b0f4736dc98fd1d2667
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-05 18:05:30 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,6 +1,6 @@
 def addition(num1, num2)
-  sum = num1 + num2
-  return sum
+  sum1 = num1 + num2
+  return sum1
 end
 
 def subtraction(num1, num2)
@@ -9,11 +9,12 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-
+  product1 = num1 / num2
 end
 
 def multiplication(num1, num2)
-
+ product2 = num1 * num2
+ return product2
 end
 
 def modulo(num1, num2)
